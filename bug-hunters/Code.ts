03184b58403817accb7e0a15fe1c6eff9e7e5367
Alex Kalyvitis <alex.kalyvitis@theblueground.com>
2023-03-06 18:07:50 +0100

    chore: format constants

--- bug-hunters/Code.ts
+++ bug-hunters/Code.ts
@@ -1,8 +1,11 @@
 import Slack from "./Slack";
 import BugHunter from "./BugHunter";
 
-const { SLACK_OAUTH_TOKEN, SLACK_CHANNEL, SLACK_USER_GROUP } =
-  PropertiesService.getScriptProperties().getProperties();
+const { 
+  SLACK_OAUTH_TOKEN, 
+  SLACK_CHANNEL, 
+  SLACK_USER_GROUP 
+} = PropertiesService.getScriptProperties().getProperties();
 
 function notifyBugHunter() {
   const slack = new Slack(SLACK_OAUTH_TOKEN);
